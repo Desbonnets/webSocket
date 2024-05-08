7fc7658b8d7c578974bad88655d964512f75dfd4
Gilles DESBONNETS <gillesdesbonnets@gmail.com>
2024-05-08 14:52:59 +0200

    envoi de message et reception de message

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -8,32 +8,67 @@ builder.WebHost.UseUrls("http://localhost:6001");
 var app = builder.Build();
 app.UseWebSockets();
 
+var connections = new List<WebSocket>(); 
+
 app.Map("/ws", async context =>
 {
     if (context.WebSockets.IsWebSocketRequest)
     {
-        using var ws = await context.WebSockets.AcceptWebSocketAsync();
-        while (true)
-        {
-            var message = "The current time is : " + DateTime.Now.ToString("HH:mm:ss");
-            var bytes = Encoding.UTF8.GetBytes(message);
-            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+        var curName = context.Request.Query["name"];
 
-            if(ws.State == WebSocketState.Open)
+        using var ws = await context.WebSockets.AcceptWebSocketAsync();
+
+        connections.Add(ws);
+
+        await Broadcast($"{curName} joined the room");
+        await Broadcast($"{connections.Count} users connected");
+        await ReceiveMessage(
+            ws,
+            async (result, buffer) =>
             {
-                await ws.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
-            }
-            else if (ws.State == WebSocketState.Closed || ws.State == WebSocketState.Aborted)
-            {
-                break;
-            }
-            Thread.Sleep(1000);
-        }
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    await Broadcast(curName + ": " + message);
+                }
+                else if (result.MessageType == WebSocketMessageType.Close || ws.State == WebSocketState.Aborted)
+                {
+                    connections.Remove(ws);
+                    await Broadcast($"{curName} left the room");
+                    await Broadcast($"{connections.Count} users connected");
+                    await ws.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+                }
+            });
     }
     else
     {
         context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
     }
 });
+
+async Task ReceiveMessage(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+{
+    var buffer = new byte[1024 * 4];
+
+    while (socket.State == WebSocketState.Open)
+    {
+        var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        handleMessage(result, buffer);
+    }
+}
+
+async Task Broadcast(string message)
+{
+    var bytes = Encoding.UTF8.GetBytes(message);
+
+    foreach (var socket in connections)
+    {
+        if(socket.State == WebSocketState.Open)
+        {
+            var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
+            await socket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+}
 
 await app.RunAsync();
